17844b840c4080e8c5db4c3860b47772d1379d01
Ytakagi66 <ptlv66httl@gmail.com>
2021-02-14 20:41:14 +0900

    新規登録/ユーザー情報_単体コード（できる時）

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,53 +5,60 @@ RSpec.describe User, type: :model do
     @user = FactoryBot.build(:user)
   end
   describe '新規登録/ユーザー情報' do
-    it 'ニックネームが空だと登録できない' do
-      @user.nickname = ''
-      @user.valid?
-      expect(@user.errors.full_messages).to include("Nickname can't be blank")
+    context '新規登録できる時' do
+      it '全て正常に登録できる' do
+        expect(@user).to be_valid
+      end
     end
-    it 'メールアドレスが空だと登録できない' do
-      @user.email = ''
-      @user.valid?      
-      expect(@user.errors.full_messages).to include("Email can't be blank")
-    end
-    it 'メールアドレスが重複していると登録できない' do
-      @user.save
-      another_user = FactoryBot.build(:user)
-      another_user.email = @user.email
-      another_user.valid?
-      expect(another_user.errors.full_messages).to include("Email has already been taken")
-    end
-    it 'メールアドレスに@が含まれていなければ登録できない' do
-      @user.email = 'test.com'
-      @user.valid?
-      expect(@user.errors.full_messages).to include("Email is invalid")
-    end
-    it 'パスワードが空だと登録できない' do
-      @user.password = ''
-      @user.valid?
-      expect(@user.errors.full_messages).to include("Password can't be blank")
-    end
-    it 'パスワードが、6文字未満だと登録できない' do
-      @user.password = '12345'
-      @user.valid?
-      expect(@user.errors.full_messages).to include("Password is too short (minimum is 6 characters)")
-    end
-    it 'パスワードが半角英数字混合でなければ登録できない' do
-      @user.password = '123456'
-      @user.valid?
-      expect(@user.errors.full_messages).to include("Password is invalid")
-    end
-    it '確認用のパスワードがなければ登録できない' do
-      @user.password_confirmation = ''
-      @user.valid?
-      expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")    
-    end
-    it 'パスワードとパスワード（確認用）の値が異なると登録できない' do
-      @user.password = 'a123456'
-      @user.password_confirmation = 'b123456'
-      @user.valid?
-      expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+    context '新規登録できない時' do
+      it 'ニックネームが空だと登録できない' do
+        @user.nickname = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Nickname can't be blank")
+      end
+      it 'メールアドレスが空だと登録できない' do
+        @user.email = ''
+        @user.valid?      
+        expect(@user.errors.full_messages).to include("Email can't be blank")
+      end
+      it 'メールアドレスが重複していると登録できない' do
+        @user.save
+        another_user = FactoryBot.build(:user)
+        another_user.email = @user.email
+        another_user.valid?
+        expect(another_user.errors.full_messages).to include("Email has already been taken")
+      end
+      it 'メールアドレスに@が含まれていなければ登録できない' do
+        @user.email = 'test.com'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Email is invalid")
+      end
+      it 'パスワードが空だと登録できない' do
+        @user.password = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password can't be blank")
+      end
+      it 'パスワードが、6文字未満だと登録できない' do
+        @user.password = '12345'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password is too short (minimum is 6 characters)")
+      end
+      it 'パスワードが半角英数字混合でなければ登録できない' do
+        @user.password = '123456'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password is invalid")
+      end
+      it '確認用のパスワードがなければ登録できない' do
+        @user.password_confirmation = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")    
+      end
+      it 'パスワードとパスワード（確認用）の値が異なると登録できない' do
+        @user.password = 'a123456'
+        @user.password_confirmation = 'b123456'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+      end
     end
   end
 end

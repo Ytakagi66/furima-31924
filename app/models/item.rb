1c96c9b0144525f541ba845cd6e7b14c38390141
Ytakagi66 <ptlv66httl@gmail.com>
2021-02-16 21:23:12 +0900

    newファイルのActiveHashの編集

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,12 +1,23 @@
 class Item < ApplicationRecord
   belongs_to :user
 
-  extend ActiveHash::Associations::ActiveRecordExtensions
+  VALID_PRICE_REGEX = /\A[0-9]+\z/.freeze
+  
+  validates :name, :info, :price, presence: true
+
+  with_options numericality: { other_than: 1 } do
+    validates :category_id
+    validates :status_id
+    validates :shipping_fee_id
+    validates :prefecture_id
+    validates :scheduled_id
+  end
+
+  extend ActiveHash::Associations::ActiveRecordExtensions do
   belongs_to :category
   belongs_to :prefecture
   belongs_to :scheduled
   belongs_to :shipping_fee
   belongs_to :status
-
   end
 end

06f067154d2114e5d223880df0525e701255f295
Ytakagi66 <ptlv66httl@gmail.com>
2021-02-16 21:56:55 +0900

    バリデーションの編集

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,13 +2,13 @@ class Item < ApplicationRecord
   belongs_to :user
 
   VALID_PRICE_REGEX = /\A[0-9]+\z/.freeze
-  
+
   validates :name, :info, :price, presence: true
 
   with_options numericality: { other_than: 1 } do
     validates :category_id
     validates :status_id
-    validates :shipping_fee_id
+    validates :shipping_fee_id, format: { with: VALID_PRICE_REGEX },numericality: { only_integer: true, greater_than: 300, less_than: 9999999 }
     validates :prefecture_id
     validates :scheduled_id
   end

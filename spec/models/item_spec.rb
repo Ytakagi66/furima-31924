ec983aa400eb2af385554ddcc229f44753213bec
Ytakagi66 <ptlv66httl@gmail.com>
2021-02-20 09:33:29 +0900

    Update item_spec.rb

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -87,7 +87,7 @@ RSpec.describe Item, type: :model do
       it '価格の範囲が、¥300より安いと出品できない' do
         @item.price = 100
         @item.valid?
-        expect(@item.errors.full_messages).to include('Price must be greater than 300')
+        expect(@item.errors.full_messages).to include('Price must be greater than 299')
       end
       it '価格の範囲が、¥9,999,999を超えていると出品できない' do
         @item.price = 10_000_000

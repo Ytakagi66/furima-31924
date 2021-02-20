85dcce21dab38552778c9eb4018c39b308ba92e7
Ytakagi66 <ptlv66httl@gmail.com>
2021-02-20 16:35:51 +0900

    RuboCopの実施

--- app/models/item.rb
+++ app/models/item.rb
@@ -21,9 +21,9 @@ class Item < ApplicationRecord
   end
 
   extend ActiveHash::Associations::ActiveRecordExtensions
-    belongs_to :category
-    belongs_to :prefecture
-    belongs_to :scheduled
-    belongs_to :shipping_fee
-    belongs_to :status
+  belongs_to :category
+  belongs_to :prefecture
+  belongs_to :scheduled
+  belongs_to :shipping_fee
+  belongs_to :status
 end

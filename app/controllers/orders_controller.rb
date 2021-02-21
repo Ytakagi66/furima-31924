7086b9a749c3a2f2a29bf5a8a22199bad0e86a8a
Ytakagi66 <ptlv66httl@gmail.com>
2021-02-22 00:09:03 +0900

    orderコントローラーの編集

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,24 @@
 class OrdersController < ApplicationController
+  include ActiveModel::Model
+  attr_accessor :number, :cvc, :exp_month, :exp_year, :postal_code, :prefecture, :city, :address, :building, :phone_number
+
   def index
+    @order = Order.new
   end
 
   def create
+    @order = Order.new(order_params)
+    if @order.valid?
+      @order.save
+      return redirect_to root_path
+    else
+      render 'index'
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:price)
   end
 end

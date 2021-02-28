0f74d2f01be33895b89e1ea041fb1632e2e6f2c8
Ytakagi66 <ptlv66httl@gmail.com>
2021-02-28 21:07:49 +0900

    商品購入機能の実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,7 +2,7 @@ class OrdersController < ApplicationController
 
   def index
     @order = Order.new
-    @item = Item.find(params[:item_id])
+    set_item
     @order_shipping = OrderShipping.new
   end
 
@@ -13,18 +13,30 @@ class OrdersController < ApplicationController
   def create
     @order_shipping = OrderShipping.new(shipping_params)
     if @order_shipping.valid?
-      @order_shipping.save
+      set_item
+      Payjp.api_key = ENV["PAYJP_SECRET_KEY"]  # PAY.JPテスト秘密鍵の設定
+      Payjp::Charge.create(
+        amount: @item.price,  # 商品の値段
+        card: shipping_params[:token],    # カードトークン
+        currency: 'jpy'                 # 通貨の種類（日本円）
+      )
+      binding.pry
+      @order_shipping.save　# order_shipping.rbに記述されたsaveメソッド
      redirect_to root_path
     else
-      @item = Item.find(params[:item_id])
+      set_item
       render :index
     end
   end
 
   private
 
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
   def shipping_params
-    params.permit(:number, :exp_month, :exp_year, :cvc, :postal_code, :city, :address, :building, :phone_number, :prefecture_id, :item_id).merge(user_id: current_user.id)
+    set_item
+    params.require(:order_shipping).permit(:postal_code, :city, :address, :building, :phone_number, :prefecture_id).merge(user_id: current_user.id, token: params[:token], item_id: @item.id)
   end
 end
-
--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,5 +1,5 @@
 const pay = () => {
-  Payjp.setPublicKey("pk_test_8d7fab4e5bb9054b1e643213"); // PAY.JPテスト公開鍵
+  Payjp.setPublicKey(process.env.PAYJP_PUBLIC_KEY); // PAY.JPテスト公開鍵
   const form = document.getElementById("charge-form");
   form.addEventListener("submit", (e) => {
     e.preventDefault();
@@ -20,10 +20,10 @@ const pay = () => {
         const tokenObj = `<input value=${token} name='token' type="hidden">`;
         renderDom.insertAdjacentHTML("beforeend", tokenObj);
       }
-      document.getElementById("card_number").removeAttribute("name");
-      document.getElementById("card_cvc").removeAttribute("name");
-      document.getElementById("card_exp_month").removeAttribute("name");
-      document.getElementById("card_exp_year").removeAttribute("name");
+      document.getElementById("card-number").removeAttribute("name");
+      document.getElementById("card-exp-month").removeAttribute("name");
+      document.getElementById("card-exp-year").removeAttribute("name");
+      document.getElementById("card-cvc").removeAttribute("name");
 
       document.getElementById("charge-form").submit();
     });

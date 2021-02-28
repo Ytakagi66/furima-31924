37f00d00b5b9be9c8b014dec2121d3bbba0718a8
Ytakagi66 <ptlv66httl@gmail.com>
2021-02-28 11:55:34 +0900

    トークン化の処理

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,8 +1,24 @@
 const pay = () => {
+  Payjp.setPublicKey("pk_test_8d7fab4e5bb9054b1e643213"); // PAY.JPテスト公開鍵
   const form = document.getElementById("charge-form");
   form.addEventListener("submit", (e) => {
     e.preventDefault();
-    console.log("フォーム送信時にイベント発火")
+    const formResult = document.getElementById("charge-form");
+    const formData = new FormData(formResult);
+
+    const card = {
+      number: formData.get("order_shipping[number]"),
+      cvc: formData.get("order_shipping[cvc]"),
+      exp_month: formData.get("order_shipping[exp_month]"),
+      exp_year: `20${formData.get("order_shipping[exp_year]")}`,
+    };
+    
+    Payjp.createToken(card, (status, response) => {
+      if (status == 200) {
+        const token = response.id;
+        console.log(token)
+      }
+    });
   });
 };
 
